cee64708b0e5c2e20a351ea97dda657b9e08539a
mattiacoppini <mattiacoppini@gmail.com>
2021-10-04 13:37:50 +0200

    commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # another-blog
 
 This is just another blog!
-I'm fond of side projects that I'll never complete!
+I'm fond of side projects that I'll never complete.
---
+++ src/components/ui/Card/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Box = styled.div`
+  display: flex;
+  width: 300%;
+`;
+
+const Card = ({header, value}) => {
+
+  return (
+    <Box>
+      a
+    </Box>
+  )
+}
+
+export default Card;
